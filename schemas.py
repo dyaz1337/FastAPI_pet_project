15efe7aaccb45fae7297c42569ce89f8393869b5
dyaz1337 <iVader1488@yandex.ru>
2024-05-03 15:05:38 +0200

    fix: update schemas.py

--- schemas.py
+++ schemas.py
@@ -11,7 +11,7 @@ class STaskAdd(BaseModel):
 class STask(STaskAdd):
     id: int
 
-    m#odel_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True)
 
 
 class STaskId(BaseModel):
